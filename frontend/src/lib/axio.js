99a8eb91e15548bc6bd43d66dd40fb6fc6fd8a3d
Arti Dwivedi <artidwivedi551@gmail.com>
2026-06-25 08:25:15 +0530

    Fix API routes

--- frontend/src/lib/axio.js
+++ frontend/src/lib/axio.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://thoughtboard-3.onrender.com//api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://thoughtboard-3.onrender.com/api";
 
 
 const api = axios.create({
