f9889d215223e7056700a0b3b6ccc4ec0c1bb29a
Arti Dwivedi <artidwivedi551@gmail.com>
2026-06-25 07:40:56 +0530

    Remove Upstash dependency

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -10,7 +10,5 @@ dotenv.config();
 const ratelimit = new Ratelimit ({
     redis: Redis.fromEnv(),
     limiter: Ratelimit.slidingWindow(100, "60 s"),
-});
-
+});           
 export default ratelimit;
-
